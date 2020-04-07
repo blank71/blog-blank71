eb5dc5728806d8c3bb48d1cb628c2cae5df1dfae
blank71 <koblank18@gmail.com>
2020-04-07 17:42:22 +0900

    add hugo-new.py (can make new posts)

---
+++ hugo-new.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+template = '''---
+title: "{year}{month}{day}-{title}"
+author: ""
+type: ""
+date: {year}-{month}-{day}
+subtitle: ""
+image: ""
+tags: []
+---
+'''
+
+if __name__ == "__main__":
+  title = input("title:")
+  year = input("year:")
+  month = input("month:")
+  day = input("day:")
+
+
+  print(template.format(title=title, year=year, month=month, day=day))
+  template = template.format(title=title, year=year, month=month, day=day)
+
+path = 'content/post/{year}{month}{day}-{title}.md'
+path = path.format(title=title, year=year, month=month, day=day)
+
+with open(path, mode='w') as f:
+  f.write(template)
+
+with open(path) as f:
+  print(f.read())
+
+vim = "vim content/post/{year}{month}{day}-{title}.md"
+vim = vim.format(title=title, year=year, month=month, day=day)
+os.system(vim)
